59d4ac4521042d8f288261e27dfac419923ebabc
zosialenarczyk <zosia.lenarczyk1110@gmail.com>
2026-01-10 17:05:03 +0100

    create data dir

--- src/fruit_and_vegetable_disease/data.py
+++ src/fruit_and_vegetable_disease/data.py
@@ -13,6 +13,10 @@ DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 
+RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+
 test_size = 0.2
 seed = 42
 
