4b8f3a12f65c7588b59000903358a9410a16d1b7
zosialenarczyk <zosia.lenarczyk1110@gmail.com>
2026-01-11 15:47:33 +0100

    correct data test

---
+++ tests/test_data.py
@@ -0,0 +1,7 @@
+from fruit_and_vegetable_disease.data import create_datasets
+
+
+def test_create_datasets():
+    train_set, test_set = create_datasets("src/fruit_and_vegetable_disease/data/processed")
+    assert len(train_set) > 0
+    assert len(test_set) > 0
