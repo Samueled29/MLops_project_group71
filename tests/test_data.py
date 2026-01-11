1d76d319961c9e75d076b921906ef620a4296112
zosialenarczyk <zosia.lenarczyk1110@gmail.com>
2026-01-11 15:56:38 +0100

    fix data path

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,8 @@
 from fruit_and_vegetable_disease.data import create_datasets
+from pathlib import Path
 
 
 def test_create_datasets():
-    train_set, test_set = create_datasets("src/fruit_and_vegetable_disease/data/processed")
+    train_set, test_set = create_datasets("data/processed")
     assert len(train_set) > 0
     assert len(test_set) > 0
