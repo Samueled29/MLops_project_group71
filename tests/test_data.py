6074effb8743e2a49f72546c6dfe61ca04b4d0e5
zosialenarczyk <zosia.lenarczyk1110@gmail.com>
2026-01-11 15:59:52 +0100

    ruff formatting correct

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,6 @@
 from fruit_and_vegetable_disease.data import create_datasets
 
+
 def test_create_datasets():
     train_set, test_set = create_datasets("data/processed")
     assert len(train_set) > 0
